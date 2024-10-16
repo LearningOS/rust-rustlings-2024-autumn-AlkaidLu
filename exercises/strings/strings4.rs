108b63669738ce3ea6ee80b5627a2d359ceed5b5
Alkaild_Lu <140805367@qq.com>
2024-10-16 18:20:56 +0800

    solve strings

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -7,7 +7,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 fn string_slice(arg: &str) {
     println!("{}", arg);
@@ -17,14 +16,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());//一个显式方法，直接请求获取所有权。会创建一个新的 String，它是 &str 的所有权副本（深拷贝）.
+    string("nice weather".into());//通过 Into trait 来做隐式转换
+    string(format!("Interpolation {}", "Station"));
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());//Returns the lowercase equivalent of this string slice, as a new String.
 }
