ef6ca411c9a91057250d55da9844bdcb8e879d7f
Alkaild_Lu <140805367@qq.com>
2024-10-20 11:37:09 +0800

    solve macros

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,14 +3,12 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
-    ($val:expr) => {
+    };//宏规则的定义格式要求每个匹配规则都要以分号结束。
+    ($val:tt) => {//tt 类型的片段可以匹配任意的令牌树，并且它是少数可以直接用于字面量匹配的类型之一。
         println!("Look at this other macro: {}", $val);
     }
 }
